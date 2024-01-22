4d0e1e1c621149930593db7372441b2238e1ed9f
Spencer Hepworth <goldenspencerh@gmail.com>
2024-01-22 09:57:04 -0700

    initialized project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Login() {
+
+
+  return (
+    <section>
+            <input type="text" placeholder="Login"/>
+            <button>Submit</button>
+    </section>
+
+  );
+}
